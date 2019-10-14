661a2baa932c76c60153925b62bc3e563d2614cd
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2019-10-14 20:28:52 -0300

    done everything including stretches

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,36 @@
 // };
 // myFunction();
 
+const myFunction = () => {
+    return "Function was invoked!";
+}
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = (param) => {
+    return param;
+}
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (param1, param1) => {
+    return param1 + param2;
+}
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const subtract = (param1, param2) => {
+    return param1 - param2;
+}
 
 
 // Stretch
@@ -28,3 +44,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => {
+    return num * 3;
+})
